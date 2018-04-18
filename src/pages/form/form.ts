44652f124c0657e8c72db9188124f21136a0e249
aluno <aluno>
2018-04-18 13:02:43 -0300

    Adicionado o alert quando tento voltar do formulário para a homepage

--- src/pages/form/form.ts
+++ src/pages/form/form.ts
@@ -1,12 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the FormPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -15,7 +8,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class FormPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
@@ -23,7 +16,17 @@ export class FormPage {
   }
 
   onGoBack() {
-    this.navCtrl.popToRoot();
+    let alert = this.alertCtrl.create({
+      title: 'Tem certeza?',
+      subTitle: 'Caso volte agora, todo seu progresso no formulário será perdido.',
+      buttons: [{
+        text: 'Sim, tenho certeza!',
+        handler: () => {
+          this.navCtrl.popToRoot();
+        }
+      }, 'Não, quero ficar.']
+    });
+    alert.present();
   }
 
 }
