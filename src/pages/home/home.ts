d2e066af78fb53b51399492b10b00249c6314918
aluno <aluno>
2018-04-18 11:02:13 -0300

    Primeiro Commit, corrigir erro do lazy loading

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ModalController } from 'ionic-angular';
+import { ResultPage } from '../result/result';
+import { FormPage } from '../form/form';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,10 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController) {}
 
+  goToFormPage(){
+    this.navCtrl.push(FormPage);
   }
 
 }
