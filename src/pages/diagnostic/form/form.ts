4c7aa997d08a17aba9d3297f03c97344229e4215
aluno <aluno>
2018-04-20 14:14:06 -0300

    Adicionar campos no formulário da maneira correta

--- src/pages/diagnostic/form/form.ts
+++ src/pages/diagnostic/form/form.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
 
 import { Category } from '../../../assets/data/category.interface';
 import { Income } from '../../../assets/data/income.interface';
@@ -18,90 +18,139 @@ export class FormPage implements OnInit {
     ""
   ];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) { }
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public alertCtrl: AlertController, public formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.category = this.navParams.data;
+    this.initForm();
   }
-  
+
+  initIncomeFields(): FormGroup {
+    return this.formBuilder.group({
+      name: ['', Validators.required],
+      value: ['', Validators.required]
+    });
+  }
+
   initForm() {
     switch (this.category.name) {
       case 'Receitas': {
-        this.form =  new FormGroup({
-          
-        })
+        this.form = this.formBuilder.group({
+          income: this.formBuilder.array([
+            this.initIncomeFields()
+          ])
+        });
         break;
       }
       case 'Desembolso Fixo Obrigatório': {
-        this.form =  new FormGroup({
-          
+        this.form = new FormGroup({
+
         })
         break;
       }
       case 'Desembolso Fixo Não-Obrigatório': {
-        this.form =  new FormGroup({
-          
+        this.form = new FormGroup({
+
         })
         break;
       }
       case 'Desembolso Variável Obrigatório': {
-        this.form =  new FormGroup({
-          
+        this.form = new FormGroup({
+
         })
         break;
       }
       case 'Desembolso Variável Não-Obrigatório': {
-        this.form =  new FormGroup({
-          
+        this.form = new FormGroup({
+
         })
         break;
       }
       case 'Ativos Financeiros': {
-        this.form =  new FormGroup({
-          
+        this.form = new FormGroup({
+
         })
         break;
       }
       case 'Ativos Não-Financeiros': {
-        this.form =  new FormGroup({
-          
+        this.form = new FormGroup({
+
         })
         break;
       }
       case 'Dívidas': {
-        
+
       }
     }
   }
 
-  addField() {
+  addField(): void {
     switch (this.category.name) {
       case 'Receitas': {
-        this.numberOfFields.push("");
+        const control = <FormArray>this.form.controls.income;
+        control.push(this.initIncomeFields());
         break;
       }
       case 'Desembolso Fixo Obrigatório': {
-        
+
         break;
       }
       case 'Desembolso Fixo Não-Obrigatório': {
-        
+
         break;
       }
       case 'Desembolso Variável Obrigatório': {
-        
+
         break;
       }
       case 'Desembolso Variável Não-Obrigatório': {
-        
+
         break;
       }
       case 'Ativos Financeiros': {
-        
+
         break;
       }
       case 'Ativos Não-Financeiros': {
-        
+
+        break;
+      }
+      case 'Dívidas': {
+
+      }
+    }
+  }
+
+  removeField(i: number): void {
+    switch (this.category.name) {
+      case 'Receitas': {
+        const control = <FormArray>this.form.controls.income;
+        control.removeAt(i);
+        break;
+      }
+      case 'Desembolso Fixo Obrigatório': {
+
+        break;
+      }
+      case 'Desembolso Fixo Não-Obrigatório': {
+
+        break;
+      }
+      case 'Desembolso Variável Obrigatório': {
+
+        break;
+      }
+      case 'Desembolso Variável Não-Obrigatório': {
+
+        break;
+      }
+      case 'Ativos Financeiros': {
+
+        break;
+      }
+      case 'Ativos Não-Financeiros': {
+
         break;
       }
       case 'Dívidas': {
