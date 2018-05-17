3cc4dff836dbe400271b7fcef14f5b76bbd2322e
cleysonmagnos <cleyson_magnos1@hotmail.com>
2018-05-17 11:36:54 -0300

    Adicionando um gráfico de barras na mesma tela

--- src/pages/graficos/graficos.ts
+++ src/pages/graficos/graficos.ts
@@ -7,7 +7,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-graficos',
   templateUrl: 'graficos.html',
-  
+
 })
 
 
@@ -16,9 +16,65 @@ export class GraficosPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  public doughnutChartLabels:string[] = ['Dados 1','Dados 2', 'Dados 3'];
-  public doughnutChartData:number[] = [1500,3000,1235];
-  public doughnutChartType:string = 'doughnut';
+  public doughnutChartLabels: string[] = ['Dados 1', 'Dados 2', 'Dados 3'];
+  public doughnutChartData: number[] = [1500, 3000, 1235];
+  public doughnutChartType: string = 'doughnut';
+
+  public BarChartLabels: string[] = ["Apressados", "Com Calma", "Tranquilos"];
+  public BarChartType: string = 'bar';
+  public BarChartDataSets: object = [
+    {
+      label: 'Meus: ',
+      data: [12, 50, 20],
+      borderWidth: 1
+    },
+    {
+      label: 'Abreu: ',
+      data: [70, 30, 10],
+      borderWidth: 1
+    }
+  ];
+
+  public BarObjectOptions: object = {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  };
+
+  public barChartColors: Array<any> = [
+    {
+      backgroundColor: '#1d1de2',
+      borderColor: '#1d1de2',
+      pointBackgroundColor: '#1d1de2',
+      pointBorderColor: '#1d1de2',
+      pointHoverBackgroundColor: '#1d1de2',
+      pointHoverBorderColor: '#1d1de2',
+      labels: '#1d1de2'
+    },
+    {
+      backgroundColor: '#1d1de2',
+      borderColor: '#1d1de2',
+      pointBackgroundColor: '#1d1de2',
+      pointBorderColor: '#1d1de2',
+      pointHoverBackgroundColor: '#1d1de2',
+      pointHoverBorderColor: '#1d1de2',
+      labels: '#1d1de2'
+    },
+    {
+      backgroundColor: '#3F51B5',
+      borderColor: '#3F51B5',
+      pointBackgroundColor: '#3F51B5',
+      pointBorderColor: '#3F51B5',
+      pointHoverBackgroundColor: '#3F51B5',
+      pointHoverBorderColor: '#3F51B5',
+      labels: '#3F51B5'
+    }
+
+  ];
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad GraficosPage');
@@ -27,6 +83,6 @@ export class GraficosPage {
   onGoBack() {
     this.navCtrl.popToRoot();
   }
-  
+
 
 }
