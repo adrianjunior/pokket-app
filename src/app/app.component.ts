f838ac3e05af1ea287e7bb1981a47739de3d8d78
adrianjunior <adrianrfpj@gmail.com>
2018-06-08 15:00:48 -0300

    não lembro o que eu mudei aqui hu3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Storage } from '@ionic/storage';
 
 import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = `HomePage`;
+  @ViewChild(Nav) nav: Nav;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  rootPage: any = `HomePage`;
+
+  formListPage = `FormListPage`;
+  homePage = `HomePage`;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, storage: Storage) {
+    storage.get('Diagnostic')
+      .then(value => {
+        if (value) {
+          this.nav.setRoot(this.homePage);
+        } else {
+          this.nav.setRoot(this.formListPage);
+        }
+      })
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -18,5 +32,12 @@ export class MyApp {
       splashScreen.hide();
     });
   }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page);
+  }
+
 }
 
